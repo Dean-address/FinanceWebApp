d453348a18f5dc3d41d9dd53557998350192fed4
Alabi Khalid <alabikhalid.12@gmail.com>
2024-12-23 03:56:38 +0100

    updated prod

--- app/configs/prod.py
+++ app/configs/prod.py
@@ -3,7 +3,8 @@ from decouple import config
 import dj_database_url
 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "dean-finance.up.railway.app"]
+# ALLOWED_HOSTS = ["localhost", "127.0.0.1", "dean-finance.up.railway.app"]
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGIN = ["https://dean-finance.up.railway.app"]
 
 DATABASES = {
