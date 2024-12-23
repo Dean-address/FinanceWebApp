4c7c92557e208b2ae514cc52b55e0222003a2418
Alabi Khalid <alabikhalid.12@gmail.com>
2024-12-23 03:16:11 +0100

    deploy to railway configuration

---
+++ app/configs/prod.py
@@ -0,0 +1,11 @@
+from .base import *
+from decouple import config
+import dj_database_url
+
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "dean-finance.up.railway.app"]
+CSRF_TRUSTED_ORIGIN = ["https://dean-finance.up.railway.app"]
+
+DATABASES = {
+    "default": dj_database_url.parse(config("DB_URL")),
+}
